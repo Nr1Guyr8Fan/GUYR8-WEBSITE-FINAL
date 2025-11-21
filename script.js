da76a08cbd707cd2c7ba69ad04ca9897d5b88272
Nr1Guyr8Fan <245025837+Nr1Guyr8Fan@users.noreply.github.com>
2025-11-21 16:42:38 +0100

    commitingu desunee

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-<script data-goatcounter="https://nr1guyr8fan.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>
+<script data-goatcounter="https://nr1guyr8fan.goatcounter.com/count"
+        async src="//gc.zgo.at/count.js"></script>
